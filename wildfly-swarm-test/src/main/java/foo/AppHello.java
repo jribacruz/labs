8b797047dc8a82b35dcdb899f9cce7402822677b
jribacruz <jribacruz@gmail.com>
2016-10-16 12:52:22 -0200

    first commit

---
+++ wildfly-swarm-test/src/main/java/foo/AppHello.java
@@ -0,0 +1,16 @@
+package foo;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+
+@Path("/hello")
+public class AppHello {
+
+  @GET
+  @Produces("text/plain")
+  public Response doGet() {
+    return Response.ok("Hello from WildFly Swarm!").build();
+  }
+}
