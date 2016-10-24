8b26904af838feadc853c2f29a9e08e824f5849c
jcruz <jcruz@rpawsds01.redetre.jus.br>
2016-10-24 19:29:22 -0300

    First commit

---
+++ spring-boot-first-app-test/src/main/java/foo/App.java
@@ -0,0 +1,26 @@
+package foo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Hello world!
+ *
+ */
+@Controller
+@EnableAutoConfiguration
+public class App {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
