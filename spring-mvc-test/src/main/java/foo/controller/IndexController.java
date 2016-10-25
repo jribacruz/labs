47f7c287fa1f53ef6857469b4c5a7ec963e808ed
jribacruz <jribacruz@gmail.com>
2016-10-25 14:23:58 -0300

    first commit

---
+++ spring-mvc-test/src/main/java/foo/controller/IndexController.java
@@ -0,0 +1,24 @@
+package foo.controller;
+
+import java.io.Serializable;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class IndexController implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@RequestMapping("/")
+	public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "index";
+	}
+
+}
