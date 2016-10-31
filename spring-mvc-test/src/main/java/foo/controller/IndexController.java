51121bc45ef45de24ffc2675e92403fc835fe151
jribacruz <jribacruz@gmail.com>
2016-10-31 13:13:45 -0300

    Ajustes no controller

--- spring-mvc-test/src/main/java/foo/controller/IndexController.java
+++ spring-mvc-test/src/main/java/foo/controller/IndexController.java
@@ -5,9 +5,7 @@ import java.io.Serializable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController implements Serializable {
