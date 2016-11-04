9307f1b26ffd3c73f9c162c0ad11f5c41aa048b0
jribacruz <jribacruz@gmail.com>
2016-11-04 19:48:48 -0300

    ajuste na segurança

--- spring-security-web-test/src/main/java/foo/config/WebSecurityConfig.java
+++ spring-security-web-test/src/main/java/foo/config/WebSecurityConfig.java
@@ -1,22 +1,10 @@
 package foo.config;
 
-import java.io.IOException;
-import java.util.Collection;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.web.DefaultRedirectStrategy;
-import org.springframework.security.web.RedirectStrategy;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -36,8 +24,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.roles("MNGT");
  		// @formatter:on
 	}
-	
-	
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -48,49 +34,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.antMatchers("/user/**").hasRole("USER")
 				.anyRequest().authenticated()   
 			.and()
-			.logout()
+				.logout()
 			.and()
-			.formLogin()
+				.formLogin()
 				.successHandler(new DefaultAuthenticationSuccessHandler());
 		// @formatter:on
-
-	}
-
-	private class DefaultAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
-
-		@Override
-		public void onAuthenticationSuccess(HttpServletRequest req, HttpServletResponse res, Authentication auth)
-				throws IOException, ServletException {
-
-			if (res.isCommitted()) {
-				return;
-			}
-
-			System.out.println(auth.getAuthorities());
-			
-			Collection<? extends GrantedAuthority> authorities = auth.getAuthorities();
-			boolean isUser = false;
-			boolean isAdmin = false;
-			for (GrantedAuthority grantedAuthority : authorities) {
-				if (grantedAuthority.getAuthority().equals("ROLE_USER")) {
-					isUser = true;
-					break;
-				} else if (grantedAuthority.getAuthority().equals("ROLE_MNGT")) {
-					isAdmin = true;
-					break;
-				}
-			}
-
-			RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
-
-			if (isUser) {
-				redirectStrategy.sendRedirect(req, res, "/user/index");
-			} else if (isAdmin) {
-				redirectStrategy.sendRedirect(req, res, "/mngt/index");
-			} else {
-				throw new IllegalStateException();
-			}
-		}
 
 	}
 
