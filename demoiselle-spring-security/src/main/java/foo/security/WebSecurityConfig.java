e59ad272e443e9b3ea7219c0d1481f6b37c24379
jribacruz <jribacruz@gmail.com>
2016-11-11 21:16:11 -0300

    ajustes no http

--- demoiselle-spring-security/src/main/java/foo/security/WebSecurityConfig.java
+++ demoiselle-spring-security/src/main/java/foo/security/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package foo.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -16,4 +17,23 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		manager.createUser(User.withUsername("user").password("password").roles("USER").build());
 		return manager;
 	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http
+			.authorizeRequests()
+			.anyRequest().authenticated()
+			.and()
+				.logout().logoutSuccessUrl("/login.xhtml?logout")
+				.permitAll()
+			.and()
+				.formLogin()
+				.loginPage("/login.jsf")
+				.permitAll()
+				.defaultSuccessUrl("/index.jsf", true);
+			
+	}
+	
+	
 }
