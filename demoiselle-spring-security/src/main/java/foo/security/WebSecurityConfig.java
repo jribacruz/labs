9a6114155b6aa35c4fca0bbf23a936143ff7836f
jribacruz <jribacruz@gmail.com>
2016-11-24 21:11:39 -0300

    Ajuste no web

--- demoiselle-spring-security/src/main/java/foo/security/WebSecurityConfig.java
+++ demoiselle-spring-security/src/main/java/foo/security/WebSecurityConfig.java
@@ -1,10 +1,12 @@
 package foo.security;
 
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@EnableGlobalMethodSecurity
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
