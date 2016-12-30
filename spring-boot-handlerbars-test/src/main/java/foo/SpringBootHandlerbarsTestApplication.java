9ef01028b5279d46dff2317c4ef5c0f50e157a87
jribacruz <jribacruz@gmail.com>
2016-12-30 08:43:55 -0300

    first commit

---
+++ spring-boot-handlerbars-test/src/main/java/foo/SpringBootHandlerbarsTestApplication.java
@@ -0,0 +1,12 @@
+package foo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHandlerbarsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHandlerbarsTestApplication.class, args);
+	}
+}
