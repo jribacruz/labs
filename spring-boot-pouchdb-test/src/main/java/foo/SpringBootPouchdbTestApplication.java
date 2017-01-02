e173c84045d17f5e7e932affd17c844a76d26308
jribacruz <jribacruz@gmail.com>
2017-01-02 13:42:24 -0300

    first commit

---
+++ spring-boot-pouchdb-test/src/main/java/foo/SpringBootPouchdbTestApplication.java
@@ -0,0 +1,12 @@
+package foo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPouchdbTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPouchdbTestApplication.class, args);
+	}
+}
---
+++ spring-boot-pouchdb-test/src/main/java/foo/controller/AppController.java
@@ -0,0 +1,13 @@
+package foo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AppController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+}
