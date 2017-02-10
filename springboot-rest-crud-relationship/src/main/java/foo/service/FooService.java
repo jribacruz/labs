e485d15d5258ea9bf7c396aafddbbf0d6cfb9d85
jribacruz <jribacruz@gmail.com>
2017-02-10 17:33:51 -0300

    first commit

---
+++ springboot-rest-crud-relationship/src/main/java/foo/service/FooService.java
@@ -0,0 +1,33 @@
+package foo.service;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import foo.domain.Foo;
+import foo.repository.FooRepository;
+
+@Service
+public class FooService implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Autowired
+	private FooRepository fooRepository;
+
+	@Transactional
+	public Foo insert(Foo foo) {
+		return fooRepository.save(foo);
+	}
+
+	public List<Foo> findAll() {
+		return fooRepository.findAll();
+	}
+
+}
